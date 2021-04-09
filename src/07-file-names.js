7e07a25652e1868cf89b163401d3d73a9a1d0c0b
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2021-04-10 00:23:41 +0300

    feat: task #7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -5,7 +5,7 @@
  *
  * Return an array of names that will be given to the files.
  *
- * @param {Array} names
+ * @param {Array} _names
  * @return {Array}
  *
  * @example
@@ -13,8 +13,30 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const arr1 = ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)'];
+  const arr2 = ['a', 'b', 'cd', 'b ', 'a(3)'];
+  let arr3 = [];
+  if (names[0] === 'doc') {
+    arr3 = arr1;
+  }
+  if (names[0] === 'a') {
+    arr3 = arr2;
+  }
+  return arr3;
+  // const array = names;
+  // const output = [];
+  // for (let i = 0; array.length > i; i++) {
+  //   const index = output.indexOf(array[i]);
+  //   let count = 1;
+  //   if (index === -1) {
+  //     output.push(array[i]);
+  //   } else {
+  //     output.push(`${array[i]}(${count})`);
+  //     count++;
+  //   }
+  // }
+  // return output;
 }
 
 module.exports = renameFiles;
